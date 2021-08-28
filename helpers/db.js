138a181b1c2c002b13bd736ff623b71142c96f09
GeoBrodas <geobro2310@gmail.com>
2021-08-28 18:23:35 +0530

    security fix

--- helpers/db.js
+++ helpers/db.js
@@ -1,9 +1,14 @@
 import { MongoClient } from 'mongodb';
 
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+const cluster = process.env.MONGODB_CLUSTER;
+const collection = process.env.MONGODB_COLLECTION;
+
+const MONGODB_URI = `mongodb+srv://${username}:${password}@${cluster}.3svps.mongodb.net/${collection}?retryWrites=true&w=majority`;
+
 export async function connectToDatabase() {
-  const client = await MongoClient.connect(
-    'mongodb+srv://high_level_agent:rU5lJ4vFUfR7JR1d@cluster0.3svps.mongodb.net/authusers?retryWrites=true&w=majority'
-  );
+  const client = await MongoClient.connect(MONGODB_URI);
 
   return client;
 }
