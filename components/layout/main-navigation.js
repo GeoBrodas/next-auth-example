ba9a6b7f1bdc5d2e110c10fb5359474bf41b1a88
GeoBrodas <geobro2310@gmail.com>
2021-08-29 21:07:53 +0530

    add active session

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,26 +1,37 @@
+import { useSession } from 'next-auth/client';
 import Link from 'next/link';
 
 import classes from './main-navigation.module.css';
 
 function MainNavigation() {
+  const [session, loading] = useSession();
+
   return (
     <header className={classes.header}>
-      <Link href='/'>
+      <Link href="/">
         <a>
           <div className={classes.logo}>Next Auth</div>
         </a>
       </Link>
       <nav>
         <ul>
-          <li>
-            <Link href='/auth'>Login</Link>
-          </li>
-          <li>
-            <Link href='/profile'>Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          {!session && !loading && (
+            <li>
+              <Link href="/auth">Login</Link>
+            </li>
+          )}
+
+          {session && (
+            <li>
+              <Link href="/profile">Profile</Link>
+            </li>
+          )}
+
+          {session && (
+            <li>
+              <button>Logout</button>
+            </li>
+          )}
         </ul>
       </nav>
     </header>
